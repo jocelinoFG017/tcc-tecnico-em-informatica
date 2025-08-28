9d39e586f05364060ef795dc0d57abe6695cb50d
jocelino017 <jocelinogg@gmail.com>
2025-08-28 13:33:28 -0300

    feat: optimize database schema and update table structures for user and product management

--- src/Database/tccjocelino.sql
+++ src/Database/tccjocelino.sql
@@ -1,239 +1,301 @@
-DROP SCHEMA IF EXISTS tccjocelino;
-CREATE DATABASE tccjocelino;
-USE tccjocelino;
+-- -----------------------------------------------------
+-- Schema otimizado: tccjocelino
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `tccjocelino` DEFAULT CHARACTER SET utf8mb4;
+USE `tccjocelino`;
 
--- Database: `tccjocelino`
+-- -----------------------------------------------------
+-- NivelAcesso
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `nivelacesso` (
+  `idNivelAcesso` INT NOT NULL AUTO_INCREMENT,
+  `cargo` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`idNivelAcesso`)
+) ENGINE=InnoDB;
 
---
--- Estrutura da tabela `niveis_acessos`
--- Somente 2 usuários (o usuario padrao e o admin)
-/*
-CREATE TABLE niveisAcesso (
-  idNiveisAcesso int(11) NOT NULL auto_increment,
-  nome varchar(45) NOT NULL,
-  data date NOT NULL,
-  modified int(11) DEFAULT NULL,
-  PRIMARY KEY(idNiveisAcesso)
-) ENGINE=InnoDB DEFAULT CHARSET = UTF8 COLLATE = utf8_general_ci;
-*/
+-- -----------------------------------------------------
+-- Usuario
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `usuario` (
+  `idUsuario` INT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(100) NOT NULL,
+  `login` VARCHAR(100) NOT NULL,
+  `senha` VARCHAR(255) NOT NULL,
+  `fk_idNivelAcesso` INT NOT NULL,
+  PRIMARY KEY (`idUsuario`),
+  INDEX (`fk_idNivelAcesso`),
+  CONSTRAINT `fk_usuarioNivelAcesso`
+    FOREIGN KEY (`fk_idNivelAcesso`)
+    REFERENCES `nivelacesso`(`idNivelAcesso`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+) ENGINE=InnoDB;
 
+-- -----------------------------------------------------
+-- Categoria
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `categoria` (
+  `idCategoria` INT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(50) NOT NULL,
+  `descricao` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`idCategoria`)
+) ENGINE=InnoDB;
 
---
--- Estrutura da tabela `tipo`
---
-CREATE TABLE tipo (
-  idProduto int(11) NOT NULL auto_increment,
-  nome varchar(45) NOT NULL,
-  descricao varchar(45) NOT NULL,
-  PRIMARY KEY(idProduto)
-)ENGINE=InnoDB DEFAULT CHARSET=UTF8 COLLATE=utf8_general_ci;
+-- -----------------------------------------------------
+-- Tipo
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `tipo` (
+  `idTipo` INT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(50) NOT NULL,
+  `descricao` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`idTipo`)
+) ENGINE=InnoDB;
 
---
--- Estrutura da tabela `produto`
---
+-- -----------------------------------------------------
+-- Produto
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `produto` (
+  `idProduto` INT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(100) NOT NULL,
+  `descricao` VARCHAR(255) NOT NULL,
+  `marca` VARCHAR(100) NOT NULL,
+  `quantidade` INT NOT NULL,
+  `preco` DECIMAL(10,2) NOT NULL,
+  `foto` VARCHAR(255) NOT NULL,
+  `fk_idCategoria` INT NOT NULL,
+  `fk_idTipo` INT NOT NULL,
+  PRIMARY KEY (`idProduto`),
+  INDEX (`fk_idCategoria`),
+  INDEX (`fk_idTipo`),
+  CONSTRAINT `fk_produtoCategoria`
+    FOREIGN KEY (`fk_idCategoria`)
+    REFERENCES `categoria`(`idCategoria`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_produtoTipo`
+    FOREIGN KEY (`fk_idTipo`)
+    REFERENCES `tipo`(`idTipo`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+) ENGINE=InnoDB;
 
-CREATE TABLE produto (
-  idProduto int(11) NOT NULL auto_increment,
-  nome varchar(45) NOT NULL,
-  descricao varchar(45) NOT NULL,
-  marca varchar(45) NOT NULL,
-  quantidade varchar(45) NOT NULL,
-  preco float(45) NOT NULL,
-  foto varchar(100) NOT NULL,
-  PRIMARY KEY(idProduto)
-) ENGINE=InnoDB DEFAULT CHARSET=UTF8 COLLATE=utf8_general_ci;
+-- -----------------------------------------------------
+-- Pais
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `pais` (
+  `idPais` INT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(100) NOT NULL,
+  PRIMARY KEY (`idPais`)
+) ENGINE=InnoDB;
 
---
--- Estrutura da tabela `usuarios`
---
+-- -----------------------------------------------------
+-- Estado
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `estado` (
+  `idEstado` INT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(100) NOT NULL,
+  `uf` VARCHAR(2) NOT NULL,
+  `fk_idPais` INT NOT NULL,
+  PRIMARY KEY (`idEstado`),
+  INDEX (`fk_idPais`),
+  CONSTRAINT `fk_estadoPais`
+    FOREIGN KEY (`fk_idPais`)
+    REFERENCES `pais`(`idPais`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+) ENGINE=InnoDB;
 
-CREATE TABLE usuario (
-    idUsuario int(11) NOT NULL auto_increment,
-    nome varchar(45) NOT NULL,
-    login varchar(45) NOT NULL,
-    senha varchar(45) NOT NULL,-- id niveis d acesso
-    PRIMARY KEY(idUsuario)
-    -- idNiveisAcesso int,
-   -- CONSTRAINT fk_NiveisAcesso FOREIGN KEY(idNiveisAcesso) REFERENCES usuario(idUsuario)
-)CHARSET=utf8;
---   CONSTRAINT fk_NiveisAcesso FOREIGN KEY (idNiveisAcesso) REFFERENCES usuario(idUsuario)
--- Normalização do endereco
+-- -----------------------------------------------------
+-- Cidade
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `cidade` (
+  `idCidade` INT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(100) NOT NULL,
+  `fk_idEstado` INT NOT NULL,
+  PRIMARY KEY (`idCidade`),
+  INDEX (`fk_idEstado`),
+  CONSTRAINT `fk_cidadeEstado`
+    FOREIGN KEY (`fk_idEstado`)
+    REFERENCES `estado`(`idEstado`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+) ENGINE=InnoDB;
 
--- Estrutura da tabela `Pais`
+-- -----------------------------------------------------
+-- Endereco
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `endereco` (
+  `idEndereco` INT NOT NULL AUTO_INCREMENT,
+  `bairro` VARCHAR(100) NOT NULL,
+  `rua` VARCHAR(100) NOT NULL,
+  `numero` VARCHAR(20) NOT NULL,
+  `telefone` BIGINT NOT NULL,
+  `fk_idCidade` INT NOT NULL,
+  PRIMARY KEY (`idEndereco`),
+  INDEX (`fk_idCidade`),
+  CONSTRAINT `fk_enderecoCidade`
+    FOREIGN KEY (`fk_idCidade`)
+    REFERENCES `cidade`(`idCidade`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+) ENGINE=InnoDB;
 
-CREATE TABLE pais(
-    idPais int(11) NOT NULL auto_increment,
-    nome varchar(45) NOT NULL,
-    PRIMARY KEY (idPais)
-)CHARSET=utf8;
-
--- Estrutura da tabela `estado`
-
-CREATE TABLE estado(
-    idEstado int(11) NOT NULL auto_increment,
-    nome varchar(45) NOT NULL,
-    UF char(2) NOT NULL,
-    idPais int,
-    PRIMARY KEY (idEstado),
-    CONSTRAINT fk_Pais FOREIGN KEY (idPais) REFERENCES pais(idPais)
-)CHARSET=utf8;
-
--- Estrutura da tabela `cidade`
-
-CREATE TABLE cidade(
-    idCidade int(11) NOT NULL auto_increment,
-    nome varchar(45) NOT NULL,
-    idEstado int,
-    PRIMARY KEY (idCidade)
-    -- CONSTRAINT fk_Estado FOREIGN KEY(idEstado) REFERENCES estado(idEstado)
-)CHARSET=utf8;
--- Estrutura da tabela `endereco`
-
-CREATE TABLE endereco (
-    idEndereco int(11) NOT NULL auto_increment,
-    bairro varchar(45) NOT NULL,
-    rua varchar(45) NOT NULL,
-    numero varchar(45) NOT NULL,
-    cidadeId int NULL,
-    estadoID int NULL,
-    telefone varchar(20) NOT NULL,
-    -- idPais int NULL,
-    PRIMARY KEY (idEndereco),
-    CONSTRAINT fk_EnderecoCidade FOREIGN KEY (cidadeId) REFERENCES cidade(idCidade),
-   CONSTRAINT fk_EnderecoEstado FOREIGN KEY (estadoId) REFERENCES estado(idEstado)
-    -- CONSTRAINT fk_EnderecoPais FOREIGN KEY (idPais) REFERENCES pais(idPais)
-)CHARSET=utf8;
-
-
-CREATE TABLE artigo(
-      idArtigo int NOT NULL auto_increment,
-      titulo varchar(45) NOT NULL,
-      texto varchar(1200) NOT NULL,
-      autor varchar(45) NOT NULL,
-      data_publicacao DATE,
-      tag varchar(45) NOT NULL,
-      sobreAutor VARCHAR(100) NOT NULL,
-      PRIMARY KEY(idArtigo)
+-- -----------------------------------------------------
+-- Artigo
+-- -----------------------------------------------------
+CREATE TABLE `artigo` (
+  `idArtigo` INT NOT NULL AUTO_INCREMENT,
+  `titulo` VARCHAR(45) NOT NULL,
+  `texto` VARCHAR(1200) NOT NULL,
+  `autor` VARCHAR(45) NOT NULL,
+  `data_publicacao` DATE,
+  `tag` VARCHAR(45) NOT NULL,
+  `sobreAutor` VARCHAR(100) NOT NULL,
+  PRIMARY KEY (`idArtigo`)
 );
 
--- POPULANO TABELAS
 
--- POPULANDO tabela `niveis_acessos`
-/*
-INSERT INTO niveisAcesso (idNiveisAcesso, nome, data, modified)
-VALUES(1, 'administrador', '1928-10-10', NULL),
-      (2, 'usuario', '1928-10-10', NULL);
-*/
+-- -----------------------------------------------------
+-- POPULANDO tabela nivelacesso
+insert into nivelacesso(idNivelAcesso, cargo)
+values 
+  (1,'administrador'),
+	(2,'usuario');
 
---  POPULANDO tabela`usuarios`
+-- -----------------------------------------------------
+-- POPULANDO tabela Usuario
+INSERT INTO usuario (nome, login, senha, fk_idNivelAcesso)
+VALUES 
+  ('administrador', 'admin@admin.com.br', md5('1234'), 1),
+  ('usuario', 'user@user.com.br', md5('123'), 2),
+  ('joe', 'joelokao@gmail.com.br', md5('123'), 2),
+  ('jenkins', 'jenkins@gmail.com.br', md5('123'), 2),
+  ('mega', 'mega@mega.com.br', md5('123'), 2);
 
-INSERT INTO usuario (idUsuario,nome,login, senha)
-VALUES (1,'administrador', 'admin@admin.com.br', md5('1234')),
-       (2,'usuario', 'user@user.com.br', md5('123')),
-       (3,'joe', 'joelokao@gmail.com.br', md5('123')),
-       (4,'jenkins', 'jenkins@gmail.com.br', md5('123')),
-       (5,'mega', 'mega@mega.com.br', md5('123'));
+-- -----------------------------------------------------
+-- POPULANDO tabela Pais
+INSERT INTO pais (nome)
+VALUES
+  ('Brasil'),
+  ('Argentina'),
+  ('Bolivia');
 
---  POPULANDO tabela `Pais`
-INSERT INTO pais(idPais, nome)
-VALUES(1,'Brasil'),
-      (2,'Argentina'),
-      (3,'Bolivia');
+-- -----------------------------------------------------
+-- POPULANDO tabela Categoria
+INSERT INTO categoria (nome, descricao)
+VALUES
+  ('Cães', 'Produtos para cães'),
+  ('Gatos', 'Produtos para gatos'),
+  ('Pássaros', 'Produtos para pássaros'),
+  ('Peixes', 'Produtos para peixes'),
+  ('Roedores', 'Produtos para roedores'),
+  ('Acessórios', 'Acessórios em geral'),
+  ('Brinquedos', 'Brinquedos em geral'),
+  ('Medicamentos', 'Medicamentos em geral'),
+  ('Higiene', 'Produtos de higiene em geral');
 
---  POPULANDO tabela `Estado`
-INSERT INTO estado (idEstado, nome, sigla, paisId) VALUES
-(1,'Acre','AC',1),
-(2,'Alagoas','AL',1),
-(3,'Amapá','AP',1),
-(4,'Amazonas','AM',1),
-(5,'Bahia','BA',1),
-(6,'Ceará','CE',1),
-(7,'Distrito Federal','DF',1),
-(8,'Espírito Santo','ES',1),
-(9,'Goiás','GO',1),
-(10,'Maranhão','MA',1),
-(11,'Mato Grosso','MT',1),
-(12,'Mato Grosso do Sul','MS',1),
-(13,'Minas Gerais','MG',1),
-(14,'Pará','PA',1),
-(15,'Paraíba','PB',1),
-(16,'Paraná','PR',1),
-(17,'Pernambuco','PE',1),
-(18,'Piauí','PI',1),
-(19,'Rio de Janeiro','RJ',1),
-(20,'Rio Grande do Norte','RN',1),
-(21,'Rio Grande do Sul','RS',1),
-(22,'Rondônia','RO',1),
-(23,'Roraima','RR',1),
-(24,'Santa Catarina','SC',1),
-(25,'São Paulo','SP',1),
-(26,'Sergipe','SE',1),
-(27,'Tocantins','TO',1);
+-- -----------------------------------------------------
+-- POPULANDO tabela Tipo
+INSERT INTO tipo (nome, descricao)
+VALUES
+  ('Racao', 'Rações para cães, gatos, peixes e roedores'),
+  ('Alimento Umido', 'Alimentos enlatados ou úmidos para pets'),
+  ('Suplemento / Vitamina', 'Suplementos e vitaminas para pets'),
+  ('Brinquedo', 'Brinquedos para cães, gatos e outros animais'),
+  ('Coleira / Acessorio', 'Coleiras, guias e acessórios em geral'),
+  ('Vacina', 'Vacinas para prevenção de doenças'),
+  ('Medicamento', 'Medicamentos para tratamento de pets'),
+  ('Higiene', 'Produtos de higiene e cuidados'),
+  ('Cama / Casa', 'Camas, casinhas e locais de descanso'),
+  ('Acessorio para aquario', 'Filtros, decorações e acessórios para peixes');
 
+-- -----------------------------------------------------
+-- POPULANDO tabela Estado
+INSERT INTO estado (nome, uf, fk_idPais) VALUES
+('Acre','AC',1),
+('Alagoas','AL',1),
+('Amapa','AP',1),
+('Amazonas','AM',1),
+('Bahia','BA',1),
+('Ceara','CE',1),
+('Distrito Federal','DF',1),
+('Espirito Santo','ES',1),
+('Goias','GO',1),
+('Maranhao','MA',1),
+('Mato Grosso','MT',1),
+('Mato Grosso do Sul','MS',1),
+('Minas Gerais','MG',1),
+('Para','PA',1),
+('Paraiba','PB',1),
+('Parana','PR',1),
+('Pernambuco','PE',1),
+('Piaui','PI',1),
+('Rio de Janeiro','RJ',1),
+('Rio Grande do Norte','RN',1),
+('Rio Grande do Sul','RS',1),
+('Rondonia','RO',1),
+('Roraima','RR',1),
+('Santa Catarina','SC',1),
+('Sao Paulo','SP',1),
+('Sergipe','SE',1),
+('Tocantins','TO',1);
 
---  POPULANDO tabela `Cidade`
-INSERT INTO cidade(idCidade, nome, idEstado) VALUES
-(1,'Uruguaiana', 1),
-(2,'Porto Alegre', 1),
-(3,'Itaqui', 1),
-(4,'Blumenau', 2),
-(5,'Florianópolis', 2),
-(6,'Curitiba', 3),
-(7,'Londrina', 3),
-(8,'Vitória', 8),
-(9,'Vila Velha', 8),
-(10,'Belo Horizonte', 13),
-(11,'Uberlândia', 13),
-(12,'São Paulo', 25),
-(13,'Campinas', 25),
-(14,'Rio de Janeiro', 19),
-(15,'Niterói', 19),
-(16,'Porto Velho', 22),
-(17,'Ji-Paraná', 22),
-(18,'Boa Vista', 23),
-(19,'Manaus', 4),
-(20,'Belém', 14),
-(21,'Santarém', 14),
-(22,'Recife', 17),
-(23,'Olinda', 17),
-(24,'Fortaleza', 6),
-(25,'Caucaia', 6),
-(26,'Natal', 20),
-(27,'Mossoró', 20),
-(28,'João Pessoa', 15),
-(29,'Campina Grande', 15),
-(30,'Aracaju', 26),
-(31,'Nossa Senhora do Socorro', 26),
-(32,'Teresina', 18),
-(33,'Parnaíba', 18),
-(34,'Goiânia', 9),
-(35,'Aparecida de Goiânia', 9),
-(36,'Palmas', 27);
+-- -----------------------------------------------------
+-- POPULANDO tabela Cidade
+INSERT INTO cidade (nome, fk_idEstado) VALUES
+('Uruguaiana',21),
+('Porto Alegre',21),
+('Itaqui',21),
+('Blumenau',2),
+('Florianopolis',2),
+('Curitiba',3),
+('Londrina',3),
+('Vitoria',8),
+('Vila Velha',8),
+('Belo Horizonte',13),
+('Uberlandia',13),
+('Sao Paulo',25),
+('Campinas',25),
+('Rio de Janeiro',19),
+('Niteroi',19),
+('Porto Velho',22),
+('Ji-Parana',22),
+('Boa Vista',23),
+('Manaus',4),
+('Belem',14),
+('Santarem',14),
+('Recife',17),
+('Olinda',17),
+('Fortaleza',6),
+('Caucaia',6),
+('Natal',20),
+('Mossoro',20),
+('Joao Pessoa',15),
+('Campina Grande',15),
+('Palmas',27);
 
+-- -----------------------------------------------------
+-- POPULANDO tabela Endereco
+INSERT INTO endereco (bairro, rua, numero, telefone, fk_idCidade)
+VALUES
+('Onedo Carvalho','Mariquita',2312,55991514169,1),
+('Vila Isabel','Maria Candida',4212,55991514439,2);
 
+-- -----------------------------------------------------
+-- POPULANDO tabela Produto
+INSERT INTO produto (nome, descricao, marca, quantidade, preco, foto, fk_idCategoria, fk_idTipo)
+VALUES
+('Racao Pedigree','Racao para cachorros','Pedigree',5,24.99,'939dd63bebed93eef687566b8a28aa1e.jpg',1,1),
+('Racao Pedigree Biscrok Adulto','Racao para cachorros','Pedigree',5,34.99,'0d74792eaaccadb05a022026646d294e.jpg',1,1),
+('Racao Pedigree Carne e Vegetais','Racao para cachorros','Pedigree',5,19.99,'725363c7cd502e54716899dd609f5f3a.jpg',1,1),
+('Racao Pedigree Adulto','Racao para cachorros','Pedigree',9,59.15,'939dd63bebed93eef687566b8a28aa1e.jpg',1,1),
+('Racao Zorro Adulto','Racao para cachorros','Zorro',5,25.15,'a40d63c0428b75cd5b563e23a8b82afe.jpg',1,1),
+('Racao Zorro Filhote','Racao para cachorros','Zorro',7,27.15,'a40d63c0428b75cd5b563e23a8b82afe.jpg',1,1),
+('Racao Pedigree Biscrok Filhote','Racao para cachorros','Pedigree',5,24.99,'0d74792eaaccadb05a022026646d294e.jpg',1,1),
+('Racao MegaZOO','Racao para cachorros','MegaZOO',3,45.22,'e737f1f818f5482684dbee524567008d.jpg',1,1);
 
---  POPULANDO tabela `endereco`
--- INSERT INTO endereco(idEndereco,bairro,rua,numero,idCidade,idEstado,idPais,telefone)
-INSERT INTO endereco(idEndereco,bairro,rua,numero,cidadeId,estadoId,telefone)
-VALUES(1,'Onedo Carvalho','mariquita',2312,1,1,55991514169),
-      (2,'Vila Isabel','Maria Candida',4212,2,1,55991514439);
-      -- (3,'Jardim da paz','aparicio mariense',9928,3,1,1,55921514469),
-      -- (4,'centro','barao do rio branco',8273,4,1,1,55991514269),
-      -- (5,'andradas','eusébio martins',0192,5,1,1,55991514462);
-
---  POPULANDO tabela `produto`
-INSERT INTO produto(idProduto,nome,descricao,marca,quantidade,preco,foto)
-VALUES(1,'Ração Pedigree','Ração para cachorros','Pedigree','5kg',24.99,'939dd63bebed93eef687566b8a28aa1e.jpg'),
-      (2,'Ração Pedigree Biscrok Adulto','Ração para cachorros','Pedigree','5kg',34.99,'0d74792eaaccadb05a022026646d294e.jpg'),
-      (3,'Ração Pedigree Carne e Vegetais','Ração para cachorros','Pedigree','5kg',19.99,'725363c7cd502e54716899dd609f5f3a.jpg'),
-      (4,'Ração Pedigree Adulto','Ração para cachorros','Pedigree','9kg',59.15,'939dd63bebed93eef687566b8a28aa1e.jpg'),
-      (5,'Ração Zorro Adulto','Ração para cachorros','Zorro','5kg',25.15,'a40d63c0428b75cd5b563e23a8b82afe.jpg'),
-      (6,'Ração Zorro Filhote','Ração para cachorros','Zorro','7kg',27.15,'a40d63c0428b75cd5b563e23a8b82afe.jpg'),
-      (7,'Ração Pedigree Biscrok Filhote','Ração para cachorros','Pedigree','5kg',24.99,'0d74792eaaccadb05a022026646d294e.jpg'),
-      (8,'Ração MegaZOO','Ração para cachorros','MegaZOO','3kg',45.22,'e737f1f818f5482684dbee524567008d.jpg');
-
-
-
---  POPULANDO tabela `curiosidades`
-INSERT INTO artigo(idArtigo, titulo, texto, autor, data_publicacao, tag, sobreAutor)
-VALUES (1,'O destino dos cães', 'Os cães nascem para encontrar seu elo perdido', 'Jocelino F.G','2020-12-12','Gatos' ,'Nascido no RS');
+-- -----------------------------------------------------
+-- POPULANDO tabela Artigo
+INSERT INTO artigo (titulo, texto, autor, data_publicacao, tag, sobreAutor)
+VALUES
+('O destino dos cães', 'Os cães nascem para encontrar seu elo perdido', 'Jocelino F.G','2020-12-12','Gatos','Nascido no RS');
