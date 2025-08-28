c3de0d0376c781e154290505f01cd45674af642b
jocelino017 <jocelinogg@gmail.com>
2025-08-28 11:01:06 -0300

    feat: update page titles and footer links for consistency and navigation

--- src/templates/footer.php
+++ src/templates/footer.php
@@ -7,7 +7,7 @@
                 <h5 class="text-warning">Serviços</h5>
                 <ul class="list-unstyled">
                     <li><a href="../Listar/contato.php" class="text-light text-decoration-none">Contato</a></li>
-                    <li><a href="#" class="text-light text-decoration-none">FAQ’s</a></li>
+                    <li><a href="../pages/faq.php" class="text-light text-decoration-none">FAQ’s</a></li>
                 </ul>
             </div>
 
@@ -15,8 +15,8 @@
             <div class="col-md-2 col-6 mb-4">
                 <h5 class="text-warning">Políticas</h5>
                 <ul class="list-unstyled">
-                    <li><a href="#" class="text-light text-decoration-none">Termos de Uso</a></li>
-                    <li><a href="#" class="text-light text-decoration-none">Política de Privacidade</a></li>
+                    <li><a href="../pages/termosDeUso.php" class="text-light text-decoration-none">Termos de Uso</a></li>
+                    <li><a href="../pages/politicaPrivacidade.php" class="text-light text-decoration-none">Política de Privacidade</a></li>
                 </ul>
             </div>
 
